d1599598addddc901f495dbc17e813af118d2512
DimiFrag7 <dimifrag7@pme.duth.gr>
2024-10-02 10:11:08 +0300

    Updated

--- src/demo_world/src/demo.py
+++ src/demo_world/src/demo.py
@@ -1,70 +1,80 @@
 #!/usr/bin/env python3
 
-import rospy, tf, rospkg, random
+import rospy, tf, rospkg
 from gazebo_msgs.srv import DeleteModel, SpawnModel, GetModelState
 from geometry_msgs.msg import Quaternion, Pose, Point
 
 class CubeSpawner():
 
-	def __init__(self) -> None:
-		self.rospack = rospkg.RosPack()
-		self.path = self.rospack.get_path('demo_world')+"/urdf/"
-		self.cubes = []
-		self.cubes.append(self.path+"red_cube.urdf")
-		self.cubes.append(self.path+"green_cube.urdf")
-		self.cubes.append(self.path+"blue_cube.urdf")
-		self.col = 0
+    def __init__(self) -> None:
+        self.rospack = rospkg.RosPack()
+        self.path = self.rospack.get_path('demo_world') + "/urdf/"
+        self.cubes = []
+        self.cubes.append(self.path + "red_cube.urdf")
+        self.cubes.append(self.path + "green_cube.urdf")
+        self.cubes.append(self.path + "blue_cube.urdf")
+        self.col = 0
 
-		self.sm = rospy.ServiceProxy("/gazebo/spawn_urdf_model", SpawnModel)
-		self.dm = rospy.ServiceProxy("/gazebo/delete_model", DeleteModel)
-		self.ms = rospy.ServiceProxy("/gazebo/get_model_state", GetModelState)
+        self.sm = rospy.ServiceProxy("/gazebo/spawn_urdf_model", SpawnModel)
+        self.dm = rospy.ServiceProxy("/gazebo/delete_model", DeleteModel)
+        self.ms = rospy.ServiceProxy("/gazebo/get_model_state", GetModelState)
 
-	def checkModel(self):
-		res = self.ms("cube", "world")
-		return res.success
+    def checkModel(self):
+        try:
+            res = self.ms("cube", "world")
+            return res.success
+        except rospy.ServiceException:
+            return False
 
-	def getPosition(self):
-		res = self.ms("cube", "world")
-		return res.pose.position.z
+    def getPosition(self):
+        try:
+            res = self.ms("cube", "world")
+            return res.pose.position
+        except rospy.ServiceException:
+            return None
 
-	def spawnModel(self):
-		# print(self.col)
-		cube = self.cubes[self.col]
-		with open(cube,"r") as f:
-			cube_urdf = f.read()
-		
-		quat = tf.transformations.quaternion_from_euler(0,0,0)
-		orient = Quaternion(quat[0],quat[1],quat[2],quat[3])
-		pose = Pose(Point(x=0,y=-0.55,z=0.75), orient)
-		self.sm("cube", cube_urdf, '', pose, 'world')
-		if self.col<2:
-			self.col += 1
-		else:
-			self.col = 0
-		rospy.sleep(1)
+    def spawnModel(self):
+        cube = self.cubes[self.col]
+        with open(cube, "r") as f:
+            cube_urdf = f.read()
 
-	def deleteModel(self):
-		self.dm("cube")
-		rospy.sleep(1)
+        quat = tf.transformations.quaternion_from_euler(0, 0, 0)
+        orient = Quaternion(quat[0], quat[1], quat[2], quat[3])
+        pose = Pose(Point(x=1.0, y=-0.55, z=0.75), orient)
+        self.sm("cube", cube_urdf, '', pose, 'world')
+        self.col = (self.col + 1) % len(self.cubes)
+        rospy.sleep(1)
 
-	def shutdown_hook(self):
-		self.deleteModel()
-		print("Shutting down")
+    def deleteModel(self):
+        try:
+            self.dm("cube")
+            rospy.sleep(1)
+        except rospy.ServiceException:
+            pass
 
+    def shutdown_hook(self):
+        self.deleteModel()
+        print("Shutting down")
 
 if __name__ == "__main__":
-	print("Waiting for gazebo services...")
-	rospy.init_node("spawn_cubes")
-	rospy.wait_for_service("/gazebo/delete_model")
-	rospy.wait_for_service("/gazebo/spawn_urdf_model")
-	rospy.wait_for_service("/gazebo/get_model_state")
-	r = rospy.Rate(15)
-	cs = CubeSpawner()
-	rospy.on_shutdown(cs.shutdown_hook)
-	while not rospy.is_shutdown():
-		if cs.checkModel()==False:
-			cs.spawnModel()
-		elif cs.getPosition()<0.05:
-			cs.deleteModel()
-		r.sleep()
-		
+    print("Waiting for gazebo services...")
+    rospy.init_node("spawn_cubes")
+    rospy.wait_for_service("/gazebo/delete_model")
+    rospy.wait_for_service("/gazebo/spawn_urdf_model")
+    rospy.wait_for_service("/gazebo/get_model_state")
+    r = rospy.Rate(15)
+    cs = CubeSpawner()
+    rospy.on_shutdown(cs.shutdown_hook)
+    while not rospy.is_shutdown():
+        if not cs.checkModel():
+            cs.spawnModel()
+        else:
+            position = cs.getPosition()
+            if position is not None:
+                if position.z < 0.05:
+                    cs.deleteModel()
+                elif position.x < 0.7:
+                    rospy.sleep(12)
+                    cs.deleteModel()
+                    
+        r.sleep()
